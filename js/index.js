34ec77ca2975c90979e9af29127c3ad548389fa4
oauka <77268885+oauka@users.noreply.github.com>
2021-03-04 16:21:39 +0900

    ver 6.0

--- js/index.js
+++ js/index.js
@@ -9,10 +9,11 @@ $(".slide-group").slick({
 
 $('.article1 .plpa').on('click', function(){
     // .addClass() : 클래스 추가
-    // .removeclass() : 클래스 삭제
+    // .removeClass() : 클래스 삭제
     // .hasClass() : 클래스 유무 판단(결과는 true 또는 false)
+    // if ( true or false ) { true 실행문 } else { false 실행문 }
     var $ibutton = $(this).find('i')
-    if ( $ibutton.hasClass('fa-pause') ) {
+    if ( $ibutton.hasClass('fa-pause') ) { 
         $('.slide-group').slick('slickPause')
         $ibutton.removeClass('fa-pause').addClass('fa-play')
     } else {
@@ -23,65 +24,92 @@ $('.article1 .plpa').on('click', function(){
 
 // .index() : 선택자의 인덱스번호를 알아냄
 // p.60~61 참고
-// 변수선언 var 변수명;
+// 변수선언 : var 변수명
 var num;
 $('.cs_board .tabmenu > li').on('click', function(){
     $(this).addClass('active')
     .siblings().removeClass('active')
     // console.log( $(this).index() )
-    num = $(this).index()
-    // 변수에 값 저장하기 : 변수값 = 값
-    // 변수에는 값을 하나만 저장할 수 있음
+    // 변수에 값 저장하기 : 변수명 = 값
+    // 변수는 값을 하나만 저장할 수 있음
     // 새로운 값을 저장하면 이전값은 지워짐
-    console.log(num)
+    num = $(this).index()
     $(this).parent().next().children()
-    .eq().addClass('active')
+    .eq(num).addClass('active')
     .siblings().removeClass('active')
 })
 
-// $('.cs_board .contents > div')
-    // $(this).parent().next().children('div')
-    // .eq($(this).index()).addClass('active')
-    // .siblings().removeClass('active')
 
 
-var ww = $(window).width()
 
-if (ww>=1025) {
-    $('#nav .depth1 > li').hover(
-        function(){
-            $(this).addClass('on')
-        },
-        function(){
-            $(this).removeClass('on')
-        }
-    )
-} else {
-    $('#nav .depth1 > li').on('click', function(){
-        $(this).toggleClass('on')
-        $(this).siblings().removeClass('on')
+// 동영상이미지 클릭시 모달창에서 유튜브동영상 플레이시키기
+$('.tubewrap img').on('click', function(){
+    $('body').append('<div class="vout"><div class="vin"><iframe width="100%" height="100%" src="https://www.youtube.com/embed/h7C3RyiZfYs?controls=0&amp;mute=1&amp;autoplay=1&amp;rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><button>닫기</button></div></div>')
+    $('.vout').css({
+        position:'fixed',
+        top:0, left:0, right:0, bottom:0,
+        background:'rgba(0,0,0,0.8)',
+        zIndex:'99999999'
+    }) 
+    $('.vin').css({
+        position:'absolute',
+        top:'100px', left:'100px', right:'100px', bottom:'100px'
     })
-}
-
-$('#header .open').on('click', function(){
-    $(this).next().css({
-        display:'block'
-    })
-    $(this).css({
-        display:'none'
-    })
-    $(this).next().next().css({
-        display:'block'
+    $('.vin button').css({
+        position:'absolute',
+        top:'-10px', right:'-10px',
+        padding:'5px',
+        background:'#fff',
+        color:'#000',
+        borderRadius:'5px'
     })
 })
-$('#header .close').on('click', function(){
-    $(this).prev().css({
-        display:'none'
-    })
-    $(this).css({
-        display:'none'
-    })
-    $(this).prev().prev().css({
-        display:'block'
-    })
+
+$('body').on('click', '.vout', function(){
+    $(this).remove()
 })
+$('body').on('click', '.vin button', function(){
+    $('.vout').remove()
+})
+
+
+
+// What We Do에서 클릭시 배경이미지 모달창에 표시하기
+$('.article2 ul li').on('click', function(){
+    var bgimg = $(this).css('backgroundImage')
+    var newbg = bgimg.split('img/')
+    var src = newbg[1].replace('")', '')
+    console.log(src)
+   $('body').append(`<div class="outbox"><div class="inbox"></div></div>`)
+   $('.outbox').css({
+       position:'fixed',
+       top:0, left:0, right:0, bottom:0,
+       background:'rgba(0,0,0,0.8)'
+   })
+   $('.inbox').css({
+    position:'absolute',
+    top:'50%', left:'50%', transform:'translate(-50%,-50%)'
+    })
+    $('.inbox').append(`<img src="./img/${src}" alt="">`)
+})
+
+
+
+// product 구역의 제품설명을 두 줄만 보이도록 하시오.
+// p.129 .substring() 메서드 활용
+// 제이쿼리 $('선택자')에는 .textContent 속성을 사용할 수 없으나,
+// 자바스크립트 오브젝트 elP[i] 에는 제이쿼리 .text() 메서드를 사용할 수 있음
+$('.article4 ul p').each(function(){
+    var text = $(this).text()
+    text = text.substring(0, 57)
+    $(this).text(text+'...')
+})
+
+// var elP = document.querySelectorAll('.article4 ul p')
+// for (var i=0; i<elP.length; i++) {
+//     var text = elP[i].textContent
+//     text = text.substring(0, 57)
+//     elP[i].textContent = text+'...'
+// }
+
+
